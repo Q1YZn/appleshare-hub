6b9e6adae3c3397df846d91e8a659e1a300ff4f4
Q1YZn <Q1YZn@users.noreply.github.com>
2026-08-12 16:17:18 +0800

    chore: remove footer API and Apple support links

--- frontend/src/components/AppFooter.vue
+++ frontend/src/components/AppFooter.vue
@@ -1,7 +1,5 @@
 <template>
   <footer class="footer">
     <span>AppleShare Hub</span>
-    <a href="/api/accounts" target="_blank" rel="noreferrer">API 响应</a>
-    <a href="https://support.apple.com/zh-cn/118294" target="_blank" rel="noreferrer">Apple 官方支持</a>
   </footer>
 </template>
